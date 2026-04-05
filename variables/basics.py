2ca68018223282345a4c7564de2dabfadd42d53d
dskvamsi <dskvamsi24@gmail.com>
2026-04-05 15:22:42 +0530

    learning about basic I/O

--- variables/basics.py
+++ variables/basics.py
@@ -1,2 +1,40 @@
-a="Hello world, by vamsi"
-print(a)
+a="vamsi"
+print(a)   # Hello world, by vamsi
+print(type(a))
+
+a='pranav'
+a=10
+print(type(a))  # pranav
+print(id(a))  # the id function is used to
+# give you the memory address of the varible
+
+b=10.0
+print(b)
+print(type(b)) 
+
+push_opt_in=True 
+push_opt_out=False
+
+
+print(type(push_opt_out))
+
+name='vamsi'
+father_name='raghu'
+mother_name='chandrakala'
+wife_name=None 
+print(wife_name)
+
+# camel_case,snake_case,pascal_case
+
+is_boolean=True  #snakecase
+
+isBoolean=True  #camelcase
+
+IsBoolean=True #pascalcase
+
+DevaSriKrishnaVamsi='pascal case'
+deva_sri_krishna_vamsi='snake case'
+devaSriKrishnaVamsi='camelcase'
+
+
+print(deva_sri_krishna_vamsi)
---
+++ variables/input.py
@@ -0,0 +1,12 @@
+# This file is responsible for taking user-input operations
+
+name = input("Enter your name ")
+print("my name is "+name)
+
+print("printing 5 table")
+n=int(input("Enter the number "))
+print(n)
+
+#taking user input and printing the table 
+for i in range(1,11):
+    print(f'{n} * {i} = {n*i}')
