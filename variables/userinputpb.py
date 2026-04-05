63c5eb8920a67263f5ab5fa03285803ed040ed76
dskvamsi <dskvamsi24@gmail.com>
2026-04-05 20:15:44 +0530

    basic pbs

---
+++ variables/userinputpb.py
@@ -0,0 +1,19 @@
+# Write a program that asks the user for their name and age, then prints:
+# "Hello <name>, you are <age> years old!"
+
+# Take two numbers as input and print their sum, difference, product, and quotient.
+
+# name= input("Enter your name " )
+# print(name)
+# age = int(input("Enter Your Age " ))
+# print(age)
+# print (f"Hello {name}, you are {age} years old!")
+
+
+a = int(input("Enter the  value of  a "))
+b = int (input("Enter the  value of b"))
+
+print (a+b)
+print (a-b)
+print (a*b)
+print (a/b)
