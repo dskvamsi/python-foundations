db5bba1002ddda650fef7a427afd83896c8f361a
dskvamsi <dskvamsi24@gmail.com>
2026-04-10 15:40:28 +0530

    problems solving

--- variables/trainticket.py
+++ variables/trainticket.py
@@ -3,7 +3,7 @@ age = int(input("Enter the age "))
 if (age <12):
     print ("pay the pay full fare. ")
 
-elif 12<= age >=60:
+elif 12<= age <=60:
     print("you get 50% discount on fare. ")
 
 else:
